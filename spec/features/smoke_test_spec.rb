0f192de58895e783a765706373c8fc72275936b3
Oren Kanner <okanner@gmail.com>
2016-10-13 11:10:16 -0400

    update test

--- spec/features/smoke_test_spec.rb
+++ spec/features/smoke_test_spec.rb
@@ -3,6 +3,6 @@ require "spec_helper"
 describe "Smoke test", type: :feature do
   it "works" do
     visit "/"
-    expect(page).to have_content("Hello world!")
+    expect(page).to have_content("Hello Yale!")
   end
 end
